0056357ef5a4d9e0e6e4eaa1917d27e331b91e0e
Nadawev <nrlz90@yahoo.com>
2017-04-14 12:58:27 +0300

    Add files via upload

---
+++ src/dsf.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Nadav on 4/13/2017.
+ */
+public class dsf {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
